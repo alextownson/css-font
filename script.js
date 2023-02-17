af6ae96def900722aac8f4629467108baaa6ce11
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-02-17 15:13:51 -0500

    created git for css font

---
+++ script.js
@@ -0,0 +1,394 @@
+console.log('CSS Alphabet')
+
+const a = document.getElementById('a'),
+    b = document.getElementById('b'),
+    c = document.getElementById('c'),
+    d = document.getElementById('d'),
+    e = document.getElementById('e'),
+    f = document.getElementById('f'),
+    g = document.getElementById('g'),
+    h = document.getElementById('h'),
+    i = document.getElementById('i'),
+    j = document.getElementById('j'),
+    k = document.getElementById('k'),
+    l = document.getElementById('l'),
+    m = document.getElementById('m'),
+    n = document.getElementById('n'),
+    o = document.getElementById('o'),
+    p = document.getElementById('p'),
+    q = document.getElementById('q'),
+    r = document.getElementById('r'),
+    s = document.getElementById('s'),
+    t = document.getElementById('t'),
+    u = document.getElementById('u'),
+    v = document.getElementById('v'),
+    w = document.getElementById('w'),
+    x = document.getElementById('x'),
+    y = document.getElementById('y'),
+    z = document.getElementById('z'),
+    white = document.getElementsByClassName('white')
+;
+
+// A
+
+a.addEventListener("mouseover", ()=>{
+    for (var i = 0; i < 3; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+a.addEventListener("mouseout", ()=>{
+    for (var i = 0; i < 3; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// B
+
+b.addEventListener("mouseover", ()=>{
+    for (var i = 3; i < 6; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+b.addEventListener("mouseout", ()=>{
+    for (var i = 3; i < 6; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// C
+
+c.addEventListener("mouseover", ()=>{
+    for (var i = 6; i < 9; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+c.addEventListener("mouseout", ()=>{
+    for (var i = 6; i < 9; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// D
+
+d.addEventListener("mouseover", ()=>{
+    for (var i = 9; i < 10; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+d.addEventListener("mouseout", ()=>{
+    for (var i = 9; i < 10; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// E
+
+e.addEventListener("mouseover", ()=>{
+    for (var i = 10; i < 14; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+e.addEventListener("mouseout", ()=>{
+    for (var i = 10; i < 14; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// F
+
+f.addEventListener("mouseover", ()=>{
+    for (var i = 14; i < 18; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+f.addEventListener("mouseout", ()=>{
+    for (var i = 14; i < 18; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// G
+
+g.addEventListener("mouseover", ()=>{
+    for (var i = 18; i < 22; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+g.addEventListener("mouseout", ()=>{
+    for (var i = 18; i < 22; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// H
+
+h.addEventListener("mouseover", ()=>{
+    for (var i = 22; i < 25; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+h.addEventListener("mouseout", ()=>{
+    for (var i = 22; i < 25; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// I
+
+i.addEventListener("mouseover", ()=>{
+    for (var i = 25; i < 28; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+i.addEventListener("mouseout", ()=>{
+    for (var i = 25; i < 28; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// J
+
+j.addEventListener("mouseover", ()=>{
+    for (var i = 28; i < 32; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+j.addEventListener("mouseout", ()=>{
+    for (var i = 28; i < 32; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// K
+
+k.addEventListener("mouseover", ()=>{
+    for (var i = 32; i < 38; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+k.addEventListener("mouseout", ()=>{
+    for (var i = 32; i < 38; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// L
+
+l.addEventListener("mouseover", ()=>{
+    for (var i = 38; i < 41; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+l.addEventListener("mouseout", ()=>{
+    for (var i = 38; i < 41; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// M
+
+m.addEventListener("mouseover", ()=>{
+    for (var i = 41; i < 47; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+m.addEventListener("mouseout", ()=>{
+    for (var i = 41; i < 47; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// N
+
+n.addEventListener("mouseover", ()=>{
+    for (var i = 47; i < 50; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+n.addEventListener("mouseout", ()=>{
+    for (var i = 47; i < 50; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// O
+
+o.addEventListener("mouseover", ()=>{
+    for (var i = 50; i < 51; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+o.addEventListener("mouseout", ()=>{
+    for (var i = 50; i < 51; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// P
+
+p.addEventListener("mouseover", ()=>{
+    for (var i = 51; i < 54; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+p.addEventListener("mouseout", ()=>{
+    for (var i = 51; i < 54; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// Q
+
+q.addEventListener("mouseover", ()=>{
+    for (var i = 54; i < 56; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+q.addEventListener("mouseout", ()=>{
+    for (var i = 54; i < 56; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// R
+
+r.addEventListener("mouseover", ()=>{
+    for (var i = 56; i < 61; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+r.addEventListener("mouseout", ()=>{
+    for (var i = 56; i < 61; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// S
+
+s.addEventListener("mouseover", ()=>{
+    for (var i = 61; i < 66; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+s.addEventListener("mouseout", ()=>{
+    for (var i = 61; i < 66; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// T
+
+t.addEventListener("mouseover", ()=>{
+    for (var i = 66; i < 70; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+t.addEventListener("mouseout", ()=>{
+    for (var i = 66; i < 70; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// U
+
+u.addEventListener("mouseover", ()=>{
+    for (var i = 70; i < 73; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+u.addEventListener("mouseout", ()=>{
+    for (var i = 70; i < 73; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// V
+
+v.addEventListener("mouseover", ()=>{
+    for (var i = 73; i < 75; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+v.addEventListener("mouseout", ()=>{
+    for (var i = 73; i < 75; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// W
+
+w.addEventListener("mouseover", ()=>{
+    for (var i = 75; i < 81; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+w.addEventListener("mouseout", ()=>{
+    for (var i = 75; i < 81; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// X
+
+x.addEventListener("mouseover", ()=>{
+    for (var i = 81; i < 83; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+x.addEventListener("mouseout", ()=>{
+    for (var i = 81; i < 83; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// Y
+
+y.addEventListener("mouseover", ()=>{
+    for (var i = 83; i < 88; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+y.addEventListener("mouseout", ()=>{
+    for (var i = 83; i < 88; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
+
+// Z
+
+z.addEventListener("mouseover", ()=>{
+    for (var i = 88; i < 91; i++) {
+        white[i].style.backgroundColor = "red";
+    }
+});
+
+z.addEventListener("mouseout", ()=>{
+    for (var i = 88; i < 91; i++) {
+        white[i].style.backgroundColor = "white";
+    }
+});
